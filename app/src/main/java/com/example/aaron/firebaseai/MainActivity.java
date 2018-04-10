4e4c4c161d6a4461ab9e3b24a8df89482f009fb1
tim2464 <tim2464@yahoo.com.tw>
2018-04-10 10:24:59 +0800

    add many layout & something....XD

--- app/src/main/java/com/example/aaron/firebaseai/MainActivity.java
+++ app/src/main/java/com/example/aaron/firebaseai/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.aaron.firebaseai;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_sign_In, btn_register;
+
+    FirebaseAuth auth;
+    FirebaseDatabase db;
+    DatabaseReference users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
+        users = db.getReference("Users");
+
+
     }
 }
---
+++ app/src/main/java/com/example/aaron/firebaseai/Main_Fragment.java
@@ -0,0 +1,7 @@
+package com.example.aaron.firebaseai;
+
+
+import android.support.v4.app.Fragment;
+
+public class Main_Fragment extends Fragment {
+}
